f4103339f55f9051e7c9cf21ae655c7bab39379b
SM SAIF <smsaif767@gmail.com>
2024-02-04 19:06:54 +0530

    init cors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Routes from './Routes'
 
 function App() {
   axios.defaults.baseURL = 'https://chat-app-api-gi97.onrender.com'
-  axios.defaults.withCredentials = true
+  // axios.defaults.withCredentials = true
   return (
     <UserContextProvider>
       <Routes />
